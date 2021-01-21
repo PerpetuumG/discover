7490eec9942d57b8177c64f132a8bd829fc49b67
PerpetuumG <perpetuumg@mail.ru>
2021-01-21 16:07:02 +0200

    Сверстал блок Service и подключил Slick slider

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,21 @@ $(function () {
     $('.mini-menu').click(function () {
         if ($('nav ul').css('display') == 'none') {
             $('nav ul').slideDown();
-        }
-        else{
+        } else {
             $('nav ul').slideUp();
         }
+    });
+
+    $('.service-slider').slick({
+        arrows: false,
+        dots: true,
+        responsive: [{
+            breakpoint: 600,
+            settings: {
+                dots: false,
+                centerMode: true,
+                centerPadding: '50px'
+            }
+        }]
     });
 });
